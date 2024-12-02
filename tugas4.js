38811c73be2761e1c5763cff73ba646cfa6d478b
Wobi Nurdiansyah <wobinurdiansyah20@gmail.com>
2024-12-02 08:37:15 +0700

    two commit 2

--- tugas4.js
+++ tugas4.js
@@ -1,30 +1,3 @@
-// function Reverseword(string){
-// return string.split(" ").reverse().join(' ');
-// }
-// console.log(Reverseword('Saya Belajar Javascript'))
-
-// function cekPalindrom(str) {
-// const newstr1 = str.toLowerCase()
-// const newStr2 = newstr1.split('')
-// const newstr3 = '';
-// for(let i = 0; i<newStr2.length; i++){
-// if(newStr2[i] !== ' '){
-// newstr3 += newStr2[i];
-// }
-// }
-// const newstr4 = '';
-// for(let j = newstr3.length -1; j >= 0; j--){
-// newstr4 += newstr3[j];
-// }
-// if (newstr3 === newstr4) {
-// return `kata palindrom ${result}`;
-// } else {
-// return `kata bukan palindrom ${result}`;
-// }
-// }
-// console.log(cekPalindrom("Malam"));
-// console.log(cekPalindrom("guuk"));
-
 function cekpalindrome(str) {
   // Ubah semua huruf menjadi huruf kecil
   let newstr1 = str.toLowerCase();
@@ -36,12 +9,12 @@ function cekpalindrome(str) {
       newstr3 += newstr2[i];
     }
   }
-  // Buat versi terbalik dari kata tersebut
+  // versi terbalik dari teks
   let newstr4 = "";
   for (var j = newstr3.length - 1; j >= 0; j--) {
     newstr4 += newstr3[j];
   }
-  // Bandingkan kedua versi
+  // membandingkan dua versi
   if(newstr3 === newstr4){
     return `kata adalah palindrom ${str}`
   } else {
@@ -51,3 +24,10 @@ function cekpalindrome(str) {
 
 // console.log(cekpalindrome('Malam'))
 console.log(cekpalindrome('Kuda')); 
+
+//riverse balikan kata
+function Reverseword(string){
+  return string.split(" ").reverse().join(' ');
+  }
+  console.log(Reverseword('Saya Belajar Javascript'))
+  
